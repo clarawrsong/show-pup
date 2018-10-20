36b4043e5b1937d8af5c72343eb13d4d055b4a53
Clara Song <cs2274@cornell.edu>
2018-10-20 01:44:33 -0400

    react-navigation installed

--- App.js
+++ App.js
@@ -21,9 +21,8 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>showpup</Text>
+        <Text style={styles.welcome}>showpup</Text>
       </View>
     );
   }
